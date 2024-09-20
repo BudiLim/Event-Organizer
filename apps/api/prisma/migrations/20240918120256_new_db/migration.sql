cf230df0d6c193b9812f407f7cb68436a16e51cc
BudiLim <limmtwenc@gmail.com>
2024-09-20 14:46:13 +0700

    feat: pull

--- apps/api/prisma/migrations/20240918120256_new_db/migration.sql
+++ apps/api/prisma/migrations/20240918120256_new_db/migration.sql
@@ -136,6 +136,15 @@ CREATE TABLE `Points` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Feedback` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `message` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Event` ADD CONSTRAINT `Event_organizerId_fkey` FOREIGN KEY (`organizerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
